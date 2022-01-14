52004db9f439faa8b096d41c20d6e01417447d20
Jano <janosisti@gmail.com>
2022-01-14 19:57:50 -0300

    correccion del useEffect

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { useEffect } from "react/cjs/react.development";
+import { useState,useEffect } from "react";
 import { AddCategory } from "../components/AddCategory";
 import { getGifs } from "../helpers/getGifs";
 
